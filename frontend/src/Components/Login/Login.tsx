3ea5b914ccd3894a19d979d827e8bff1bd17b1c6
Pujakumari1511 <puja.kumari6494@gmail.com>
2025-03-06 14:08:34 +0200

    Add css for loin page

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useAuth } from "../../Contexts/AuthContext";
+import './Login.css'
 
 const Login = () => {
 	const { login } = useAuth();
 
 	return (
-		<div>
+		<div className={"login-container"}>
 			<h1>Login Page</h1>
+			<input className={"login-input"} placeholder="  example@.com"></input>
+			
+			<button className={"login-button"} onClick={login}>Log In</button>
+
 			<p>Please log in to continue.</p>
-			<button onClick={login}>Log In</button>
 		</div>
 	);
 };
