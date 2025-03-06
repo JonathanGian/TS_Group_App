c3829cf01a6d60d50a88c5f544a23c92bacf7a58
Beliakov Vladimir <ermegilius@gmail.com>
2025-03-06 12:30:08 +0200

    add: overlay template

---
+++ frontend/src/Components/Login/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useAuth } from "../../Contexts/AuthContext";
+
+const Login = () => {
+	const { login } = useAuth();
+
+	return (
+		<div>
+			<h1>Login Page</h1>
+			<p>Please log in to continue.</p>
+			<button onClick={login}>Log In</button>
+		</div>
+	);
+};
+
+export default Login;
