6e2f8ec5bf4835dd839e2baee0d146cf9e25f7a9
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-03-13 10:30:34 +0200

    changed UploadEmails.tsx to html

--- frontend/src/Routes/appRoutes.tsx
+++ frontend/src/Routes/appRoutes.tsx
@@ -4,10 +4,8 @@ import Login from "../Components/Login/Login";
 import Overlay from "../Components/Overlay/Overlay";
 import { useAuth } from "../Contexts/AuthContext";
 import Register from "../Components/Register/Register";
-import UploadEmails from "../Components/Font-BackComponents/UploadEmails";
-import { DynamicTable } from "../Components/Font-BackComponents/DynamicTable";
+import UploadEmails from "../Components/Font-BackComponents/UploadEmails"
 import EmailStatus from "../Components/Font-BackComponents/EmailStatus";
-
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
 	const { loggedIn } = useAuth();
 	return loggedIn ? children : <Login />;
