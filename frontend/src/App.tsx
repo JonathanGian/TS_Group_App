24281f9b5ee6595d66dec7df90ed22de4d8ff842
Jonathan Gian <153126165+JonathanGian@users.noreply.github.com>
2025-03-19 19:39:22 +0200

    Update package.json to npm run dev for app to run

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./Theme/theme";
 import "./App.css";
-import useAuthCheck from "./Hooks/useAuthCheck";
+
 
 function App() {
   const router = createRoutes();
